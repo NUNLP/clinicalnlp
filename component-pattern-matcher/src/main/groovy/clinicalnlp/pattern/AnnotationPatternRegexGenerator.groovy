b3847f012f6721b5e3fe1ec672788e98a2d99667
willthompson <will.k.t@gmail.com>
2017-01-16 22:47:14 -0600

    developing new AnnotationPattern classes

--- component-pattern-matcher/src/main/groovy/clinicalnlp/pattern/AnnotationPatternRegexGenerator.groovy
+++ component-pattern-matcher/src/main/groovy/clinicalnlp/pattern/AnnotationPatternRegexGenerator.groovy
@@ -19,6 +19,7 @@ class AnnotationPatternRegexGenerator {
     // Instance fields
     // -----------------------------------------------------------------------------------------------------------------
     Map<Class<? extends Annotation>, Character> typeMap = [:]
+    Map<Class<? extends Annotation>, Set<String>> featMap = [:]
     Integer typeCode = INIT_TYPE_CODE
     AnnotationPattern pattern
 
@@ -39,6 +40,7 @@ class AnnotationPatternRegexGenerator {
      * @return
      */
     Pattern genRegExPattern() {
+        extractTypes(this.pattern)
         return Pattern.compile(handlePattern(this.pattern))
     }
 
@@ -46,6 +48,28 @@ class AnnotationPatternRegexGenerator {
     // Private methods
     // -----------------------------------------------------------------------------------------------------------------
 
+    // extract all type and feature information from the regex tree
+    private extractTypes(AnnotationPattern pattern) {
+        switch (pattern.class) {
+            case AtomicAnnotationPattern.class:
+                Class<? extends Annotation> type = pattern.type
+                if (!typeMap.keySet().contains(type)) {
+                    typeMap[type] = (char)this.typeCode
+                    typeCode++
+                    featMap[type] = new TreeSet<String>()
+                }
+                featMap[type].addAll(pattern.features.keySet())
+                break
+            case SequenceAnnotationPattern.class:
+                pattern.children.each { extractTypes(it) }
+                break
+            case OptionAnnotationPattern.class:
+                pattern.children.each { extractTypes(it) }
+                break
+        }
+    }
+
+    // recursive method for generating regex string
     private String handlePattern(AnnotationPattern pattern) {
         switch (pattern.class) {
             case AtomicAnnotationPattern.class:
@@ -57,31 +81,36 @@ class AnnotationPatternRegexGenerator {
         }
     }
 
+    // base case method for generating regex string
     private String handlePattern(AtomicAnnotationPattern pattern) {
         Class<? extends Annotation> type = pattern.type
-        if (!typeMap.keySet().contains(type)) {
-            typeMap[type] = (char)this.typeCode
-            typeCode++
+        Map<String, String> feats = pattern.features
+        String featString = featMap[type].inject('') { prefix, featName ->
+            String featVal = feats.containsKey(featName) ? feats[featName] : "[^${LBRACK}${RBRACK}]*"
+            prefix + "${LBRACK}${featVal}${RBRACK}"
         }
-        return annotate("${typeMap[type]}", pattern.name, pattern.range)
+        return annotate("(?:${typeMap[type]}${featString})", pattern.name, pattern.range, false)
     }
 
+    // recursive method for generating regex string
     private String handlePattern(SequenceAnnotationPattern pattern) {
         String result = pattern.children.inject(''){prefix,rest -> "${prefix}${this.handlePattern(rest)}"}
         return "(?:${result})"
     }
 
+    // recursive method for generating regex string
     private String handlePattern(OptionAnnotationPattern pattern) {
         String first = this.handlePattern(pattern.children.remove(0))
         String result = pattern.children.inject(first){prefix,rest -> "${prefix}|${this.handlePattern(rest)}"}
         return "(?:${result})"
     }
 
-    private String annotate(final String baseString, final String name, final IntRange range) {
-        String result = baseString
+    // add name, group, and quantifier information to regex
+    private String annotate(final String baseRegex, final String name, final IntRange range, final boolean addGroup) {
+        String result = baseRegex
         if (range) { result = "${result}{${range.min()},${range.max()}}"}
         if (name) { result = "(?<${name}>${result})" }
-        else { result = "(?:${result})"}
+        else if (addGroup) { result = "(?:${result})"}
         return result
     }
 }
--- component-pattern-matcher/src/test/groovy/clinicalnlp/pattern/AnnotationPatternRegexGeneratorTests.groovy
+++ component-pattern-matcher/src/test/groovy/clinicalnlp/pattern/AnnotationPatternRegexGeneratorTests.groovy
@@ -40,65 +40,67 @@ class AnnotationPatternRegexGeneratorTests {
 
     @Test
     void testAtomicPatterns() {
-        AnnotationPattern pattern = $A(Token, [pos:'NN', text:'/Foo/'])
+        AnnotationPattern pattern = $A(Token, [pos:'NN', shape:'all_caps', text:/Foo/])
         AnnotationPatternRegexGenerator generator = new AnnotationPatternRegexGenerator(pattern)
         Pattern p = generator.genRegExPattern()
         log.info "Pattern: ${p.toString()}"
-        assert p.toString() == '(?:①)'
+        assert p.toString() == '(?:①‹NN›‹all_caps›‹Foo›)'
 
         pattern = (pattern * (0..3))
         generator = new AnnotationPatternRegexGenerator(pattern)
         p = generator.genRegExPattern()
         log.info "Pattern: ${p.toString()}"
-        assert p.toString() == '(?:①{0,3})'
+        assert p.toString() == '(?:①‹NN›‹all_caps›‹Foo›){0,3}'
 
         pattern = $N('n1', pattern)
         generator = new AnnotationPatternRegexGenerator(pattern)
         p = generator.genRegExPattern()
         log.info "Pattern: ${p.toString()}"
-        assert p.toString() == '(?<n1>①{0,3})'
+        assert p.toString() == '(?<n1>(?:①‹NN›‹all_caps›‹Foo›){0,3})'
     }
 
     @Test
     void testSequencePatterns() {
-        AnnotationPattern pattern1 = $A(Token, [pos:'NN', text:'/Foo/'])
-        AnnotationPattern pattern2 = $A(Sentence, [pos:'VB', text:'/Bar/'])
-        AnnotationPattern pattern3 = $A(NamedEntityMention, [pos:'VB', text:'/Bar/'])
-        AnnotationPattern pattern = (pattern1 & pattern2 & pattern3 & pattern2 & pattern1)
+        AnnotationPattern pattern1 = $A(Token, [text:/Foo/])
+        AnnotationPattern pattern2 = $A(Sentence, [text:/Bar/])
+        AnnotationPattern pattern3 = $A(NamedEntityMention, [text:/Bar/])
+        AnnotationPattern pattern4 = $A(Token, [text:/Baz/])
+
+        AnnotationPattern pattern = (pattern1 & pattern2 & pattern3 & pattern2 & pattern4)
         AnnotationPatternRegexGenerator generator = new AnnotationPatternRegexGenerator(pattern)
         Pattern p = generator.genRegExPattern()
         log.info "Pattern: ${p.toString()}"
-        assert p.toString() == '(?:(?:①)(?:②)(?:③)(?:②)(?:①))'
+        assert p.toString() == '(?:(?:①‹Foo›)(?:②‹Bar›)(?:③‹Bar›)(?:②‹Bar›)(?:①‹Baz›))'
     }
 
     @Test
     void testOptionPatterns() {
-        AnnotationPattern pattern1 = $A(Token, [pos:'NN', text:'/Foo/'])
-        AnnotationPattern pattern2 = $A(Sentence, [pos:'VB', text:'/Bar/'])
-        AnnotationPattern pattern3 = $A(NamedEntityMention, [pos:'VB', text:'/Bar/'])
+        AnnotationPattern pattern1 = $A(Token, [text:/Foo/])
+        AnnotationPattern pattern2 = $A(Sentence, [text:/Bar/])
+        AnnotationPattern pattern3 = $A(NamedEntityMention, [text:/Bar/])
         AnnotationPattern pattern = (pattern1 | pattern2 | pattern3 | pattern2 | pattern1)
         AnnotationPatternRegexGenerator generator = new AnnotationPatternRegexGenerator(pattern)
         Pattern p = generator.genRegExPattern()
         log.info "Pattern: ${p.toString()}"
-        assert p.toString() == '(?:(?:①)|(?:②)|(?:③)|(?:②)|(?:①))'
+        assert p.toString() == '(?:(?:①‹Foo›)|(?:②‹Bar›)|(?:③‹Bar›)|(?:②‹Bar›)|(?:①‹Foo›))'
     }
 
     @Test
     void testMixedPatterns() {
-        AnnotationPattern pattern1 = $A(Token, [pos:'NN', text:'/Foo/'])
-        AnnotationPattern pattern2 = $A(Sentence, [pos:'VB', text:'/Bar/'])
-        AnnotationPattern pattern3 = $A(NamedEntityMention, [pos:'VB', text:'/Bar/'])
+        AnnotationPattern pattern1 = $A(Token, [pos:'NN'])
+        AnnotationPattern pattern2 = $A(Sentence, [modality:'Assertion'])
+        AnnotationPattern pattern3 = $A(NamedEntityMention, [type:'Disease'])
         AnnotationPattern pattern = (pattern1 & pattern2 | pattern3 & pattern2 | pattern1)
         AnnotationPatternRegexGenerator generator = new AnnotationPatternRegexGenerator(pattern)
 
         Pattern p = generator.genRegExPattern()
         log.info "Pattern: ${p.toString()}"
-        assert p.toString() == '(?:(?:(?:①)(?:②))|(?:(?:③)(?:②))|(?:①))'
+        assert p.toString() == '(?:(?:(?:①‹NN›)(?:②‹Assertion›))|(?:(?:③‹Disease›)(?:②‹Assertion›))|(?:①‹NN›))'
 
         pattern = (pattern1 & (pattern2 | pattern3) & pattern2 | pattern1)
         generator = new AnnotationPatternRegexGenerator(pattern)
         p = generator.genRegExPattern()
         log.info "Pattern: ${p.toString()}"
-        assert p.toString() == '(?:(?:(?:①)(?:(?:②)|(?:③))(?:②))|(?:①))'
+        assert p.toString() == '(?:(?:(?:①‹NN›)(?:(?:②‹Assertion›)|(?:③‹Disease›))(?:②‹Assertion›))|(?:①‹NN›))'
     }
 }
