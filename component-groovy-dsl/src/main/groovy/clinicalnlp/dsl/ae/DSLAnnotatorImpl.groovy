1dd499279e15dd5c4babeaa7cb5fbf7fa0378a7b
willthompson <will.k.t@gmail.com>
2017-10-11 14:35:30 -0500

    updating gradle file dependencies

--- component-groovy-dsl/src/main/groovy/clinicalnlp/dsl/ae/DSLAnnotatorImpl.groovy
+++ component-groovy-dsl/src/main/groovy/clinicalnlp/dsl/ae/DSLAnnotatorImpl.groovy
@@ -3,12 +3,13 @@ package clinicalnlp.dsl.ae
 import clinicalnlp.dsl.DSL
 import com.google.common.io.Resources
 import groovy.util.logging.Log4j
-import org.apache.commons.io.Charsets
 import org.apache.log4j.Level
 import org.apache.uima.UimaContext
 import org.apache.uima.jcas.JCas
 import org.apache.uima.resource.ResourceInitializationException
 import org.codehaus.groovy.control.CompilerConfiguration
+
+import java.nio.charset.Charset
 
 @Log4j
 class DSLAnnotatorImpl {
@@ -25,12 +26,12 @@ class DSLAnnotatorImpl {
         try {
             log.info "Loading groovy config file: ${scriptFile}"
             URL url = Resources.getResource(scriptFile)
-            String scriptContents = Resources.toString(url, Charsets.UTF_8)
+            String scriptContents = Resources.toString(url, Charset.forName("UTF-8"))
             this.script = shell.parse(scriptContents)
             if (bindingScriptFile) {
                 log.info "Loading groovy config binding file: ${bindingScriptFile}"
                 Script bindingsScript = shell.parse(Resources.toString(
-                    Resources.getResource(bindingScriptFile), Charsets.UTF_8))
+                    Resources.getResource(bindingScriptFile), Charset.forName("UTF-8")))
                 bindingsScript.setProperty('context', context)
                 this.script.setBinding(new Binding(bindingsScript.run()))
             }
